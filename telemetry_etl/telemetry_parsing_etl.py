03da4cacba58139500f4f35d823071ce9cc510fd
Igor Vaiman <gosha.vaiman@gmail.com>
2020-12-09 01:31:01 +0300

    12 atmospheres WIP

--- telemetry_etl/telemetry_parsing_etl.py
+++ telemetry_etl/telemetry_parsing_etl.py
@@ -12,18 +12,17 @@ from sphere_log_parser import yield_log_records_as_dicts
 # for installation see https://docs.mongodb.com/manual/administration/install-community/
 # for restoring data from dump see README.md
 client = MongoClient()
-onboard_telemetry_collection = client.sphere_telemetry.from_onboard_logs
+ground_telemetry_collection = client.sphere_telemetry.from_ground_logs
 
 
 log_filenames = [
-    'log_onboard_2010.03.18_to_2012.03.12.txt',
-    'log_onboard_2012.03.12_to_2012.03.14.txt',
-    'log_onboard_2012.03.19_to_2012.03.27.txt',
-    'log_onboard_2013.02.16_to_2013.03.16.txt'
+    'log_ground_2012.03.12_to_2012.03.14.txt',
+    'log_ground_2012.03.14_to_2013.03.11.txt',
+    'log_ground_2012.03.15.txt',
 ]
 
 # store log filenames with ids (used as foreign key)
-log_filenames_collection = client.sphere_telemetry.onboard_log_filenames
+log_filenames_collection = client.sphere_telemetry.gound_log_filenames
 for id_, log_filename in enumerate(log_filenames):
     log_filenames_collection.update_one(
         filter={'id': id_},
@@ -32,7 +31,7 @@ for id_, log_filename in enumerate(log_filenames):
     )
 
 for log_filename in log_filenames:
-    log_path = f'data\\logs\\{log_filename}'
+    log_path = f'data/logs_2012_complete/{log_filename}'
     print(f'loading {log_path}...')
 
     log_filename_id = log_filenames_collection.find_one({'filename': log_filename})['id']
@@ -46,7 +45,7 @@ for log_filename in log_filenames:
         record['local_dt'] = dt
 
         try:
-            onboard_telemetry_collection.update_one(
+            ground_telemetry_collection.update_one(
                 filter={"local_dt": {"$eq": dt}},
                 update={
                     "$setOnInsert": {key: val for key, val in record.items() if val not in {np.NaN, -1}},
